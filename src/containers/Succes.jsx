db4b3b054125c690c35a16e1fa010510fce6dd51
MatiasCeb <matiasceb8812@gmail.com>
2021-07-10 03:12:48 -0300

    router config

---
+++ src/containers/Succes.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Succes = () => {
+    return (
+        <h1>Succes</h1>
+    );
+}
+
+export default Succes;
--- webpack.config.js
+++ webpack.config.js
@@ -3,54 +3,56 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: "/"
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    module: {
+        rules: [
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+            loader: 'babel-loader',
+            },
         },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-          },
+        {
+            test: /\.html$/,
+            use: [
+            {
+                loader: 'html-loader',
+            },
+            ],
+        },
+        {
+            test: /\.css$/,
+            use: [
+            {
+                loader: MiniCssExtractPlugin.loader,
+            },
+            'css-loader',
+            ],
+        },
         ],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          'css-loader',
-        ],
-      },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: './index.html',
+        }),
+        new MiniCssExtractPlugin({
+        filename: 'assets/[name].css',
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: './index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'assets/[name].css',
-    }),
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 3005,
-  },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        historyApiFallback: true,
+        port: 3005,
+    },
 };
