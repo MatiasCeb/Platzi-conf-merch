e37e89ea83098e2736c19143a78d5b8b1b110d0a
MatiasCeb <matiasceb8812@gmail.com>
2021-07-12 21:26:57 -0300

    useRef in checkout

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -18,9 +18,17 @@ const useInitialState = () => {
         });
     }
 
+    const addToBuyer = payload => {
+        setState({
+            ...state,
+            buyer: [...state.buyer, payLoad]
+        })
+    }
+
     return {
         addToCart,
         removeFromCart,
+        addToBuyer,
         state,
     };
 };
